782e99409e83bcf23e7bc7576394a9446694f671
Ayoubslh <ayoub801@gmail.com>
2025-08-08 19:05:39 +0200

    added necessary fetch funcitons with async

--- MovieApp/Finished/scripts/apifetch.js
+++ MovieApp/Finished/scripts/apifetch.js
@@ -3,8 +3,8 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_BASE = 'https://image.tmdb.org/t/p/w500';
 
 
-async function fetchPopularMovies() {
-  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+async function fetchMovies() {
+  const response = await fetch(`${BASE_URL}/movie/discover?api_key=${API_KEY}`);
   const data = await response.json();
   console.log(data.results); // Array of movie objects
   return data.results;
@@ -15,4 +15,25 @@ async function fetchMovieDetails(movieId) {
   console.log(data); // Movie details object
   return data;
 }
-fetchMovieDetails(755898)
+async function fetchPopularMovies(){
+   const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+   const data = await response.json();
+   return data.results;
+}
+
+async function fetchTop5MoviesDrama(){
+   const popularMovies = await fetchPopularMovies();
+   return popularMovies.filter(movie => movie.genres.map(genre => genre.name).includes('Drama')).slice(0, 5);
+}
+async function fetchTop5MoviesAction(){
+   const popularMovies = await fetchPopularMovies();
+   return popularMovies.filter(movie => movie.genres.map(genre => genre.name).includes('Action')).slice(0, 5);
+}
+async function fetchTop5MoviesComedy(){
+   const popularMovies = await fetchPopularMovies();
+   return popularMovies.filter(movie => movie.genres.map(genre => genre.name).includes('Comedy')).slice(0, 5);
+}
+
+
+
+export { fetchPopularMovies, fetchMovieDetails, fetchTop5MoviesDrama, fetchTop5MoviesAction, fetchTop5MoviesComedy };
